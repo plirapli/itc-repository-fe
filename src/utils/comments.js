c74106f55fc4a1ad9527fce402a9249a2a17b12f
xharf <123190123@student.upnyk.ac.id>
2023-02-15 17:57:57 +0700

    + add discussion functionality

---
+++ src/utils/comments.js
@@ -0,0 +1,22 @@
+import { authApi } from "../api/api";
+import { formatDate, formatDateWithHour } from "./dateConverter";
+
+
+// get All Discussions Comments
+const getAllDiscussionsComments = async (courseId, discussionId) => {
+  return authApi
+    .get(`/course/${courseId}/discussion/${discussionId}/comment`)
+    .then(({ data }) => {
+      return data.data.map((comment) => {
+        return {
+          ...comment,
+          fullName: comment.User.fullName,
+          createdAt: formatDateWithHour(comment.createdAt),
+          updatedAt: formatDateWithHour(comment.updatedAt),
+        };
+      });
+    })
+    .catch(({ response }) => Promise.reject(response));
+}
+
+export { getAllDiscussionsComments }
