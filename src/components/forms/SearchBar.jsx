626f6537ee00f5a42f4a822bbdba517a045a618c
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-02-18 21:32:52 +0700

    refactor search bar

--- src/components/forms/SearchBar.jsx
+++ src/components/forms/SearchBar.jsx
@@ -1,13 +1,9 @@
-const SearchBar = ({ onChange, ...props }) => {
-  return (
-    <input
-      type='text'
-      onChange={onChange}
-      value={props.value}
-      className='search-bar block w-full rounded-md border-gray-300 shadow-sm focus-primary sm:text-sm'
-      placeholder={props.placeholder || 'Cari'}
-    />
-  );
-};
+const SearchBar = (props) => (
+  <input
+    type='text'
+    className='search-bar block w-full rounded-md border-gray-300 shadow-sm focus-primary sm:text-sm'
+    {...props}
+  />
+);
 
 export default SearchBar;
