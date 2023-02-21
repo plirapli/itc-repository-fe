c6ff453202308ed6ee37644657bac6a2012c3d5a
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-02-21 22:53:54 +0700

    refactor component modal edit

--- src/pages/manageCourse/ManageChaptersPage.jsx
+++ src/pages/manageCourse/ManageChaptersPage.jsx
@@ -13,7 +13,7 @@ import { Dialog, Transition } from '@headlessui/react';
 import Button from '../../components/buttons/Button';
 import { Input, SearchBar } from '../../components/forms';
 import { ManageCourseCard } from '../../components/cards';
-import { ModalDelete } from '../../components/modal';
+import { ModalDelete, ModalForm } from '../../components/modal';
 import OverlayLoading from '../../components/overlay/OverlayLoading';
 
 const ManageChaptersPage = () => {
@@ -218,70 +218,29 @@ const ManageChaptersPage = () => {
       </Transition>
 
       {/* Edit bab dialog (modal) */}
-      <Transition appear show={isModalEditOpen} as={Fragment}>
-        <Dialog as='div' className='relative z-10' onClose={closeModalEdit}>
-          <Transition.Child
-            as={Fragment}
-            enter='ease-out duration-300'
-            enterFrom='opacity-0'
-            enterTo='opacity-100'
-            leave='ease-in duration-200'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
-          >
-            <div className='fixed inset-0 bg-black bg-opacity-25' />
-          </Transition.Child>
-          <div className='fixed inset-0 overflow-y-auto'>
-            <div className='flex min-h-full items-center justify-center p-4 text-center'>
-              <Transition.Child
-                as={Fragment}
-                enter='ease-out duration-300'
-                enterFrom='opacity-0 scale-95'
-                enterTo='opacity-100 scale-100'
-                leave='ease-in duration-200'
-                leaveFrom='opacity-100 scale-100'
-                leaveTo='opacity-0 scale-95'
-              >
-                {/* Main Container */}
-                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-lg bg-white p-6 text-left align-middle shadow-xl transition-all'>
-                  <Dialog.Title
-                    as='h3'
-                    className='text-lg font-medium leading-6 text-gray-900'
-                  >
-                    Edit Bab
-                  </Dialog.Title>
-
-                  {/* Body */}
-                  <div className='mt-2'>
-                    <form onSubmit={editChapterHandler} action=''>
-                      <Input
-                        onChange={(e) =>
-                          setSelectedChapter((prev) => ({
-                            ...prev,
-                            title: e.target.value,
-                          }))
-                        }
-                        label='Judul'
-                        value={selectedChapter.title}
-                        color='secondary'
-                        placeholder='Masukkan judul bab'
-                        required
-                      />
-                      <div className='mt-4 flex gap-2'>
-                        <Button onClick={closeModalEdit} color='gray'>
-                          Tutup
-                        </Button>
-                        <Button onClick={closeModalEdit}>Simpan</Button>
-                      </div>
-                    </form>
-                  </div>
-                </Dialog.Panel>
-                {/* End Main Container */}
-              </Transition.Child>
-            </div>
+      <ModalForm show={isModalEditOpen} title='Edit Bab'>
+        <form onSubmit={editChapterHandler} action=''>
+          <Input
+            onChange={(e) =>
+              setSelectedChapter((prev) => ({
+                ...prev,
+                title: e.target.value,
+              }))
+            }
+            label='Judul'
+            value={selectedChapter.title}
+            color='secondary'
+            placeholder='Masukkan judul bab'
+            required
+          />
+          <div className='mt-4 flex gap-2'>
+            <Button onClick={closeModalEdit} color='gray'>
+              Tutup
+            </Button>
+            <Button onClick={closeModalEdit}>Simpan</Button>
           </div>
-        </Dialog>
-      </Transition>
+        </form>
+      </ModalForm>
 
       {/* Delete bab dialog (modal) */}
       <ModalDelete
