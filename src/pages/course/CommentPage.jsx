40c91a45e4cb9c87b5e4b97544f05955700c9880
xharf <123190123@student.upnyk.ac.id>
2023-02-24 16:21:33 +0700

    fix authorization bug in comment edit & delete ui

--- src/pages/course/CommentPage.jsx
+++ src/pages/course/CommentPage.jsx
@@ -187,6 +187,7 @@ const CommentPage = ({ user }) => {
 
         {/* Komentar */}
         <CommentLists
+          user={user}
           comments={comments}
           onClickEdit={onClickEditCommentOpenModalHandler}
           onClickDelete={onClickDeleteCommentOpenModalHandler}
