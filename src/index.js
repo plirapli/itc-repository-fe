e8d976fd1c6ade23e906c6cd2e4d053da6b8884a
Rafli <rafliyulistiawan@gmail.com>
2023-03-03 23:59:01 +0700

    remove unnecessary pacakge

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider';
 import './index.css';
 import Main from './pages/Main';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -17,8 +16,3 @@ root.render(
     </React.StrictMode>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
