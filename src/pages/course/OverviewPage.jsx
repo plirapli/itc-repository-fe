c6126b9bc51a628eff0f015fc277e13faa54fe52
xharf <123190123@student.upnyk.ac.id>
2023-03-06 09:20:48 +0700

    add function to 'belajar sekarang' button at course overview

--- src/pages/course/OverviewPage.jsx
+++ src/pages/course/OverviewPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getCourseById } from '../../utils/course';
 import { useTitle } from '../../hooks';
 
@@ -21,36 +21,36 @@ const OverviewPage = () => {
 
   return (
     <>
-      <div className='w-full py-4 px-5 sm:py-6 sm:px-0 flex flex-col gap-3 sm:gap-4'>
+      <div className="w-full py-4 px-5 sm:py-6 sm:px-0 flex flex-col gap-3 sm:gap-4">
         {/* Overview */}
-        <div className='w-full flex flex-col sm:flex-row gap-3 sm:gap-4'>
+        <div className="w-full flex flex-col sm:flex-row gap-3 sm:gap-4">
           <img
-            className='w-full h-48 sm:max-w-[19rem] object-cover bg-zinc-400 rounded-lg overflow-hidden'
-            loading='lazy'
+            className="w-full h-48 sm:max-w-[19rem] object-cover bg-zinc-400 rounded-lg overflow-hidden"
+            loading="lazy"
             src={course?.image_thumbnail}
-            alt='course-thumbnail'
+            alt="course-thumbnail"
           />
-          <div className='flex flex-col items-start justify-between'>
-            <div className='flex flex-col gap-1.5'>
+          <div className="flex flex-col items-start justify-between">
+            <div className="flex flex-col gap-1.5">
               <div>
-                <h2 className='text-lg font-medium clamp'>{course?.title}</h2>
-                <div className='text-gray-dark text-sm mt-1'>
+                <h2 className="text-lg font-medium clamp">{course?.title}</h2>
+                <div className="text-gray-dark text-sm mt-1">
                   {course?.length?.chapters} Bab | {course?.length?.articles}{' '}
                   Artikel
                 </div>
               </div>
               <Tags divName={course?.Division?.divisionName} />
-              <div className='flex items-center gap-1 text-black'>
-                <UserCircleIcon className='w-5' />
-                <span className='text-sm'>{course?.User?.fullName}</span>
+              <div className="flex items-center gap-1 text-black">
+                <UserCircleIcon className="w-5" />
+                <span className="text-sm">{course?.User?.fullName}</span>
               </div>
             </div>
 
-            <div className='mt-1.5'>
-              <p className='text-sm text-gray-dark'>
+            <div className="mt-1.5">
+              <p className="text-sm text-gray-dark">
                 Dibuat pada {course?.createdAt}
               </p>
-              <p className='mt-0.5 text-sm text-gray-dark'>
+              <p className="mt-0.5 text-sm text-gray-dark">
                 Diperbarui pada {course?.updatedAt}
               </p>
             </div>
@@ -58,12 +58,14 @@ const OverviewPage = () => {
         </div>
 
         {/* CTA Btn */}
-        <ButtonMin variant='text-only'>Belajar Sekarang</ButtonMin>
+        <Link to={`/course/${id_course}/chapter/1/article/1`}>
+          <ButtonMin variant="text-only">Belajar Sekarang</ButtonMin>
+        </Link>
 
         {/* Deskripsi */}
         <div>
-          <h2 className='text-lg'>Deskripsi</h2>
-          <p className='mt-0.5 sm:mt-1'>{course?.description}</p>
+          <h2 className="text-lg">Deskripsi</h2>
+          <p className="mt-0.5 sm:mt-1">{course?.description}</p>
         </div>
       </div>
     </>
