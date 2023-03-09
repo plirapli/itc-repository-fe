9e7cae266f928a2389bb5dce5be3a851490439d1
Rafli <rafliyulistiawan@gmail.com>
2023-03-10 01:41:25 +0700

    add isfound on discussion

--- src/pages/course/DiscussionPage.jsx
+++ src/pages/course/DiscussionPage.jsx
@@ -19,7 +19,6 @@ const DiscussionPage = () => {
   const { id_course } = useParams();
   const [discussions, setDiscussions] = useState([]);
   const [filteredDiscussions, setFilteredDiscussions] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [isModalEditOpen, setIsModalEditOpen] = useState(false);
   const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
   const [discussionTmp, setDiscussionTmp] = useState({});
@@ -27,6 +26,9 @@ const DiscussionPage = () => {
   const [topicKeyword, setTopicKeyword] = useState(
     () => params.get('topic') || ''
   );
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFound, setIsFound] = useState(false);
+  useTitle('Diksusi');
 
   const closeModalDelete = () => setIsModalDeleteOpen(false); // Tutup overlay (modal) delete
   const closeModalEdit = () => setIsModalEditOpen(false); // Tutup overlay (modal) edit
@@ -100,9 +102,18 @@ const DiscussionPage = () => {
       .finally(() => setIsLoading(false));
   };
 
-  useTitle('Diksusi');
+  console.log(id_course);
   useEffect(() => {
-    getAllDiscussionsHandler();
+    getAllDiscussions(id_course)
+      .then((data) => {
+        setDiscussions([...data]);
+        setIsFound(true);
+      })
+      .catch(({ data }) => {
+        console.log(data.message);
+        setIsFound(false);
+      })
+      .finally(() => setIsLoading(false));
   }, []);
 
   useEffect(() => {
@@ -118,107 +129,117 @@ const DiscussionPage = () => {
     }
   }, [topicKeyword, discussions]);
 
-  return (
-    <>
+  if (!isLoading) {
+    return (
       <div className='w-full py-4 px-5 sm:py-6 sm:px-0'>
-        {/* Header */}
-        <div className='flex flex-col gap-3 sm:flex-row sm:justify-between sm:items-center'>
-          <h1 className='text-2xl leading-none'>Diskusi</h1>
-          <Link to='add'>
-            <ButtonMin variant='icon-right' size='small' icon={<PlusIcon />}>
-              Buat Pertanyaan
-            </ButtonMin>
-          </Link>
-        </div>
-        <div className='w-full mt-3'>
-          <SearchBar
-            placeholder='Cari Pertanyaan'
-            value={topicKeyword}
-            onChange={(e) => onTopicsChange(e.target.value)}
-          />
-        </div>
+        {isFound && (
+          <>
+            {/* Header */}
+            <div className='flex flex-col gap-3 sm:flex-row sm:justify-between sm:items-center'>
+              <h1 className='text-2xl leading-none'>Diskusi</h1>
+              <Link to='add'>
+                <ButtonMin
+                  variant='icon-right'
+                  size='small'
+                  icon={<PlusIcon />}
+                >
+                  Buat Pertanyaan
+                </ButtonMin>
+              </Link>
+            </div>
+            <div className='w-full mt-3'>
+              <SearchBar
+                placeholder='Cari Pertanyaan'
+                value={topicKeyword}
+                onChange={(e) => onTopicsChange(e.target.value)}
+              />
+            </div>
 
-        {/* List Diskusi */}
-        <DiscussionLists
-          topicKeyword={topicKeyword}
-          discussions={filteredDiscussions}
-          onClickEdit={onClickEditDiscussionOpenModalHandler}
-          onClickDelete={onClickDeleteDiscussionOpenModalHandler}
-        />
-      </div>
-
-      {/* Edit dialog (modal) */}
-      <ModalForm show={isModalEditOpen} title='Edit Pertanyaan'>
-        {/* Ini ditambahin handler onSubmit buat edit */}
-        <form onSubmit={onClickEditDiscussionHandler}>
-          {/* Judul */}
-          <div>
-            <Input
-              onChange={(e) =>
-                setDiscussionTmp((prev) => ({
-                  ...prev,
-                  title: e.target.value,
-                }))
-              }
-              label='Judul'
-              value={discussionTmp.title}
-              color='secondary'
-              placeholder='Masukkan judul pertanyaan'
-              required
+            {/* List Diskusi */}
+            <DiscussionLists
+              topicKeyword={topicKeyword}
+              discussions={filteredDiscussions}
+              onClickEdit={onClickEditDiscussionOpenModalHandler}
+              onClickDelete={onClickDeleteDiscussionOpenModalHandler}
             />
-          </div>
 
-          {/* Deskripsi */}
-          <div className='mt-2'>
-            <label htmlFor='body' className='block text-sm font-medium'>
-              Pertanyaan
-            </label>
-            <textarea
-              onChange={(e) =>
-                setDiscussionTmp((prev) => ({
-                  ...prev,
-                  body: e.target.value,
-                }))
-              }
-              id='body'
-              name='body'
-              value={discussionTmp.body}
-              rows={5}
-              className='input-secondary mt-1 block w-full rounded-md shadow-sm focus-primary sm:text-sm resize-none'
-              placeholder='Deskripsi materi'
-              required
-            />
-          </div>
+            {/* Edit dialog (modal) */}
+            <ModalForm show={isModalEditOpen} title='Edit Pertanyaan'>
+              {/* Ini ditambahin handler onSubmit buat edit */}
+              <form onSubmit={onClickEditDiscussionHandler}>
+                {/* Judul */}
+                <div>
+                  <Input
+                    onChange={(e) =>
+                      setDiscussionTmp((prev) => ({
+                        ...prev,
+                        title: e.target.value,
+                      }))
+                    }
+                    label='Judul'
+                    value={discussionTmp.title}
+                    color='secondary'
+                    placeholder='Masukkan judul pertanyaan'
+                    required
+                  />
+                </div>
 
-          <div className='mt-4 flex gap-2'>
-            <ButtonMin
-              type='button'
-              onClick={closeModalEdit}
-              color='gray'
-              size='small'
+                {/* Deskripsi */}
+                <div className='mt-2'>
+                  <label htmlFor='body' className='block text-sm font-medium'>
+                    Pertanyaan
+                  </label>
+                  <textarea
+                    onChange={(e) =>
+                      setDiscussionTmp((prev) => ({
+                        ...prev,
+                        body: e.target.value,
+                      }))
+                    }
+                    id='body'
+                    name='body'
+                    value={discussionTmp.body}
+                    rows={5}
+                    className='input-secondary mt-1 block w-full rounded-md shadow-sm focus-primary sm:text-sm resize-none'
+                    placeholder='Deskripsi materi'
+                    required
+                  />
+                </div>
+
+                <div className='mt-4 flex gap-2'>
+                  <ButtonMin
+                    type='button'
+                    onClick={closeModalEdit}
+                    color='gray'
+                    size='small'
+                  >
+                    Tutup
+                  </ButtonMin>
+                  <ButtonMin size='small'>Simpan</ButtonMin>
+                </div>
+              </form>
+            </ModalForm>
+
+            {/* Delete dialog (modal) */}
+            <ModalDelete
+              show={isModalDeleteOpen}
+              onClose={closeModalDelete}
+              onClickDelete={onClickDeleteDiscussionHandler}
+              title='Hapus Pertanyaan'
             >
-              Tutup
-            </ButtonMin>
-            <ButtonMin size='small'>Simpan</ButtonMin>
-          </div>
-        </form>
-      </ModalForm>
+              <p className='text-sm text-gray-500'>
+                Apakah anda yakin ingin menghapus pertanyaan ini?
+              </p>
+            </ModalDelete>
 
-      {/* Delete dialog (modal) */}
-      <ModalDelete
-        show={isModalDeleteOpen}
-        onClose={closeModalDelete}
-        onClickDelete={onClickDeleteDiscussionHandler}
-        title='Hapus Pertanyaan'
-      >
-        <p className='text-sm text-gray-500'>
-          Apakah anda yakin ingin menghapus pertanyaan ini?
-        </p>
-      </ModalDelete>
+            <OverlayLoading loadingState={isLoading} />
+          </>
+        )}
 
-      <OverlayLoading loadingState={isLoading} />
-    </>
-  );
+        {!isFound && <>Materi tidak ditemukan.</>}
+      </div>
+    );
+  } else return <OverlayLoading loadingState={isLoading} />;
 };
 
 export default DiscussionPage;
